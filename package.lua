3883a2462c0edf8e585c6f30c2b27b4a4c0ed33f
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-11-30 22:09:17 -0600

    Update package.lua
    Update name & desc lolz
    Dunno what version I should numerate, definitely not 1.2 cause of the added modules.

--- package.lua
+++ package.lua
@@ -1,7 +1,7 @@
 return {
-  name = "discord-bot-hosting",
-  version = "1.2.0",
-  description = "Hosting discord bot on Heroku (Remote Communications)",
+  name = "remote-comms-bot",
+  version = "1.3.0",
+  description = "A lua-based Discord bot.",
   main = "botmain.lua",
   scripts = {
     start = "botmain.lua"
