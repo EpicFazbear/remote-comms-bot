661eb506557d561447f854c1e16fd0d327b5792a
EpicFazbear <EpicFazbear@users.noreply.github.com>
2020-12-01 22:05:05 -0600

    Updates to permission changing
    Made it so that users couldn't change the permission levels of other users the same level as them or higher.
    (In the future I may add multiple levels that can run these commands)

--- functions.lua
+++ functions.lua
@@ -28,14 +28,6 @@ return function(ENV)
 			return ran and body or "[Content Deleted]"
 		end;
 
-		checkList = function(list, value)
-			for int, element in next, list do
-				if element == value then
-					return true, int
-				end
-			end
-		end;
-
 		getTag = function(id)
 			local ran, returned = pcall(function()
 				return client:getUser(id).tag
@@ -47,6 +39,59 @@ return function(ENV)
 			end
 		end;
 
+		getLevel = function(userid)
+			local level = 1
+			if checkList(blacklisted, userid) then
+				level = 0
+			end
+			if checkList(whitelisted, userid) then
+				level = 2
+			end
+			if checkList(admins, userid) then
+				level = 3
+			end
+			if owner == userid then
+				level = 4
+			end
+			return level
+		end;
+
+		checkPermissions = function(self, single, multiple)
+			local returns = {[0]="blacklisted", "a regular", "whitelisted", "an admin", "an owner"}
+			if tonumber(single) then
+				return getLevel(self) > getLevel(single), getTag(single)
+			end
+
+			if multiple then
+				multiple = multiple:toArray()
+				if #multiple ~= 0 then
+					local successful = true
+					local list1 = ""
+					local list2 = ""
+					for int, item in next, multiple do
+						if getLevel(self) <= getLevel(item.id) then
+							successful = false
+							list2 = list2 .. getTag(item.id) ..", "
+						else
+							list1 = list1 .. getTag(item.id) ..", "
+						end
+					end
+					if list1 == "" then
+						list1 = "No one.."
+					end
+					return successful, {string.sub(list1, 1, #list1 - 2), string.sub(list2, 1, #list2 - 2)}
+				end
+			end
+		end;
+
+		checkList = function(list, value)
+			for int, element in next, list do
+				if element == value then
+					return true, int
+				end
+			end
+		end;
+
 		returnListString = function(tableName)
 			local fullstring = ""
 			for _, item in next, tableName do
@@ -55,36 +100,43 @@ return function(ENV)
 			return fullstring
 		end;
 
-		modifyList = function(single, mult, tableName, adding)
+		modifyList = function(self, single, multiple, tableName, adding)
 			if tonumber(single) then
-				if adding then
-					table.insert(tableName, single)
-					return getTag(single)
-				else
-					local found, int = checkList(tableName, single)
-					if found then
-						table.remove(tableName, int)
-						return getTag(single)
-					end
-				end
-			end
-
-			mult = mult:toArray()
-			if #mult ~= 0 then
-				local fullstring = ""
-				for _, item in next, mult do
+				if checkPermissions(self, single) then
 					if adding then
-						table.insert(tableName, item.id)
-						fullstring = fullstring .. getTag(item.id) ..", "
+						table.insert(tableName, single)
+						return getTag(single)
 					else
-						local found, int = checkList(tableName, item.id)
+						local found, int = checkList(tableName, single)
 						if found then
 							table.remove(tableName, int)
-							fullstring = fullstring .. getTag(item.id) ..", "
+							return getTag(single)
 						end
 					end
 				end
-				return string.sub(fullstring, 1, #fullstring - 2)
+				return false
+			end
+
+			if multiple then
+				multiple = multiple:toArray()
+				if #multiple ~= 0 then
+					local fullstring = ""
+					for _, item in next, multiple do
+						if checkPermissions(self, item.id) then
+							if adding then
+								table.insert(tableName, item.id)
+								fullstring = fullstring .. getTag(item.id) ..", "
+							else
+								local found, int = checkList(tableName, item.id)
+								if found then
+									table.remove(tableName, int)
+									fullstring = fullstring .. getTag(item.id) ..", "
+								end
+							end
+						end
+					end
+					return string.sub(fullstring, 1, #fullstring - 2)
+				end
 			end
 		end;
 	};
