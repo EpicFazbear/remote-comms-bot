e6191543d093c027b9340ea8c3c9ed08419e6fe3
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-26 00:29:55 -0500

    Update package.lua
    Added/updated stuff

--- package.lua
+++ package.lua
@@ -1,11 +1,12 @@
 return {
   name = "remote-comms-bot",
-  version = "1.3.1",
+  version = "1.3.2",
   description = "A lua-based Discord bot.",
   main = "botmain.lua",
-  scripts = {
-    start = "botmain.lua"
-  },
+  homepage = "https://github.com/EpicFazbear/remote-comms-bot",
+	tags = {"roblox", "discord-bot", "luvit"},
+	license = "MIT",
+	author = "EpicFazbear",
   dependencies = {
     "SinisterRectus/discordia"
   },
