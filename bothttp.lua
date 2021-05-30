ef124885f2b0d2b9dd66b0fe6b21f5d9ba73a772
EpicFazbear <EpicFazbear@users.noreply.github.com>
2021-05-30 17:52:44 -0500

    Webserver - Initial commit
    Created all the functions needed for the webserver, now Beta testing!

--- bothttp.lua
+++ bothttp.lua
@@ -1,3 +1,5 @@
+-- Initialization of the HTTP webserver used by ROBLOX to retrieve data from Discord. --
+
 local http = require("http")
 local PRC = process.env
 
@@ -7,13 +9,35 @@ return function(ENV)
 
 	function http_table:Init()
 		self.Content = "Hello world\n"
-		self.Server = http.createServer(function (req, res)
-			local body = self.Content
+		self.Webhook = "" -- TODO: find webhook and set here
+		self.Server = http.createServer(function(req, res) -- req.url
+			local body
+			local passed = false
+			for _, data in pairs(req.headers) do
+				if data[1]:lower() == "password" then
+					if tostring(data[2]) == tostring(PRC.PASSWORD) then
+						passed = true
+					end
+				end
+			end
+		
 			res:setHeader("Content-Type", "text/plain")
+			if passed == true then
+				local path = req.url
+				if path == "/messages" then
+					res:setHeader("Webhook-URL", tostring(self.Webhook))
+					body = tostring(self.Content)
+				else
+					body = "Invalid request"
+				end
+			else
+				body = "Bad password"
+			end
 			res:setHeader("Content-Length", #body)
 			res:finish(body)
 		end)
 		self.Server:listen(PRC.PORT)
+		print("WebServer is now running.")
 	end
 
 	return http_table
